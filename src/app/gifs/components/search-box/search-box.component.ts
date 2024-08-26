77903159ad1a8cabfaee71099959e7845f149b5b
jesusrobot0 <jesusrobot9000@icloud.com>
2024-08-26 18:53:47 -0500

    Añade la estructura principal de la aplicación

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,23 @@
+import { GifsService } from './../../services/gifs.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput') public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private GifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.GifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+
+    console.log(this.GifsService.tagHistory);
+  }
+}
