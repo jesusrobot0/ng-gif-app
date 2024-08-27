29cb084be74b51ce58d56cb7c752e4d87a3a5ba4
jesusrobot0 <jesusrobot9000@icloud.com>
2024-08-26 23:48:36 -0500

    chore: Refactor GifsService to organize tag history

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   get tagHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string):void {
+  private organizeHistory(tag: string): void {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((_tag) => _tag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.trim().length === 0) return;
+
+    this.organizeHistory(tag);
   }
 }
